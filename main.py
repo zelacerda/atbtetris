e0ac6056aa35ddd73cceaa3ae49fd931938ca5f2
Zé Lacerda <zelacerda@gmail.com>
2023-08-05 21:15:10 -0300

    Add queue and level system

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import curses
 import time
 import random
+from copy import deepcopy
 
 from colors import Colors
 
@@ -13,11 +14,12 @@ DIRS = {
     KEY_LEFT: (-1, 0),
     KEY_RIGHT: (1, 0)
 }
-FREQ = 60
+FREQ = 100
 X_AREA = 30
 Y_AREA = 2
 WIDTH = 10
 HEIGHT = 22 # 20 plus 2 to spawn area
+SPEEDS = [100, 79, 62, 47, 36, 26, 19, 14, 9, 6]
 
 def setup(win):
     global colors, MAX_COLS, MAX_LINES
@@ -55,29 +57,73 @@ def draw_box(win, x, y, width, height):
         win.addstr(y, x+c, borders[2])
         win.addstr(y+height-1, x+c, borders[3])
 
+
+class Queue:
+
+    def __init__(self):
+        self.TYPES = {
+        "i": {"type": "i", "pos": [[-1, 0], [0, 0], [1, 0], [2, 0]]},
+        "o": {"type": "o", "pos": [[0, -1], [1, -1], [0, 0], [1, 0]]},
+        "t": {"type": "t", "pos": [[0, -1], [-1, 0], [0, 0], [1, 0]]},
+        "s": {"type": "s", "pos": [[-1, 0], [0, 0], [0, -1], [1, -1]]},
+        "z": {"type": "z", "pos": [[-1, -1], [0, -1], [0, 0], [1, 0]]},
+        "j": {"type": "j", "pos": [[-1, -1], [-1, 0], [0, 0], [1, 0]]},
+        "l": {"type": "l", "pos": [[-1, 0], [0, 0], [1, 0], [1, -1]]}
+        }
+        self.queue = self.shuffle() + self.shuffle() # Gen a bag with 14 pieces
+
+    def shuffle(self):
+        tetrimino_types = ["i", "o", "t", "s", "z", "j", "l"]
+        random.shuffle(tetrimino_types)
+        return tetrimino_types
+
+    def pop_tetrimino(self):
+        tetrimino_type = self.queue.pop(0)
+        if len(self.queue) == 7:
+            self.queue = self.queue + self.shuffle()
+        tetrimino = deepcopy(self.TYPES[tetrimino_type])
+        return tetrimino
+
+
 class Game:
     def __init__(self, win):
         self.win = win
         self.area = curses.newpad(HEIGHT, WIDTH*TILE_WIDTH+1)
+        self.info = curses.newpad(HEIGHT, 20)
+        self.queue = Queue()
+        self.queue_area = curses.newpad(15, 10)
         self.area.bkgd(" ", colors.GRAY)
-        self.draw_border()
+        self.draw_matrix_border()
+        self.draw_queue_border()
         self.t = 0
+        self.level = 1
+        self.lines = 0
         self.matrix = [[0 for _ in range(WIDTH)] for _ in range(HEIGHT)]
         self.init_tile()
 
     def init_tile(self):
         self.x = 4
         self.y = 1
-        self.tile = get_piece()
+        self.tile = self.queue.pop_tetrimino()
+        #self.tile = get_piece()
         self.move_tile(KEY_DOWN)
 
-    def draw_border(self):
+    def draw_matrix_border(self):
         draw_box(
             self.win,
             X_AREA-1,
             Y_AREA-1,
             WIDTH*TILE_WIDTH+TILE_WIDTH,
             HEIGHT
+        )
+
+    def draw_queue_border(self):
+        draw_box(
+            self.win,
+            53,
+            1,
+            12,
+            15
         )
 
     def draw_matrix(self):
@@ -147,12 +193,14 @@ class Game:
             counts = [c for c in l if c != 0]
             if len(counts) == 10: # Full line
                 self.matrix.pop(i)
+                self.lines += 1
+                self.level = int(self.lines/10)+1
                 zeros = [0 for _ in range(WIDTH)]
                 self.matrix = [zeros] + self.matrix
                 self.update_matrix()
 
     def update(self):
-        if self.t % 10 == 0:
+        if self.t % SPEEDS[self.level - 1] == 0:
             check = self.move_tile(KEY_DOWN)
             if check is False:
                 if self.y == 1:
@@ -161,7 +209,9 @@ class Game:
                     self.add_tile_to_matrix()
                     self.update_matrix()
                     self.init_tile()
-        #self.win.addstr(0, 34, f"({self.x:02},{self.y:02}) t={int(self.t / 60)}")
+                    #self.level += 1
+                    #self.level = min(self.level, len(SPEEDS))
+        self.win.addstr(0, 32, f"LEVEL:{self.level}  LINES:{self.lines}")
         self.area.erase()
         self.draw_matrix()
         self.draw_tile()
@@ -182,7 +232,7 @@ class Game:
                 now = tick
                 self.t += 1
                 self.update()
-            time.sleep(0.01)
+            time.sleep(0.001)
 
 
 def main(win):
